f9aa40840bfcee2f919734a0fcec70b3f61a332e
Lyxas <lucasgazeau@lgazeau.fr>
2021-11-14 15:40:03 +0100

    shuffle & voc 2

--- src/components/Test.vue
+++ src/components/Test.vue
@@ -39,8 +39,28 @@ export default {
       tmpArr.push(resArr[0])
       resArray.push(tmpArr)
     }
-    this.inputParams = resArray;
+    this.inputParams = this.shuffle(resArray);
   },
+  methods:{
+    //https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+    shuffle(array) {
+      let currentIndex = array.length,  randomIndex;
+
+      // While there remain elements to shuffle...
+      while (currentIndex != 0) {
+
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+
+        // And swap it with the current element.
+        [array[currentIndex], array[randomIndex]] = [
+          array[randomIndex], array[currentIndex]];
+      }
+
+      return array;
+    }
+  }
 }
 </script>
 
